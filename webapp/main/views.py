0239a9d5e4d28f965987f49f63dcecfd7bd47c2f
mfkrg <mfkrg@yandex.ru>
2023-03-01 00:15:22 +0300

    0.0.1

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h4>Проверка работы</h4>")
+
+def cart(request):
+    return HttpResponse("<h4>Проверка корзины</h4>")
+
+def catalog(request):
+    return HttpResponse("<h4>Проверка каталога</h4>")
