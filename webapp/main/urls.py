80cde33e811f84da76372b062b4900bccfa3fd92
mfkrg <mfkrg@yandex.ru>
2023-03-01 12:07:59 +0300

    0.0.2

--- webapp/main/urls.py
+++ webapp/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('cart/', views.cart),
-    path('catalog/', views.catalog)
+    path('catalog/', views.catalog),
+    path('about/', views.about)
 ]
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -8,3 +8,6 @@ def cart(request):
 
 def catalog(request):
     return HttpResponse("<h4>Проверка каталога</h4>")
+
+def about(request):
+    return HttpResponse("<h4>Проверка страницы о нас</h4>")
