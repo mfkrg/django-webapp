a38376b62928906b684ec42a9b01e3845ad332e8
mfkrg <mfkrg@yandex.ru>
2023-03-03 01:12:51 +0300

    0.1.3 Добавлен путь MEDIA_URL и MEDIA_ROOT Добавлена возможность загружать фотографии в БД TESTS -> OK

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     'C:/Users/mfkrg/PycharmProjects/django-webapp/webapp/main/static/'
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
