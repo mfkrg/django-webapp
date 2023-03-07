695057af321087b482264eb812df058fa1fd18a2
mfkrg <mfkrg@yandex.ru>
2023-03-07 22:08:26 +0300

    0.2.4 Добавлен метод регистрации

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 def index(request):
     return render(request, 'main/index.html')
 
@@ -15,5 +17,14 @@ def about(request):
 def auth(request):
     return render(request, 'main/auth.html')
 
-def register(request):
-    pass
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Регистрация успешна.")
+            return redirect("main:index")
+        messages.error(request, "Что-то пошло не так.")
+    form = NewUserForm()
+    return render(request=request, template_name="main/auth.html", context={"register_form":form})
